133811748906ec170e01c35b3874ec38130e0649
Jeff Swanner <jeffswanner93@gmail.com>
2019-10-09 22:16:50 -0400

    heroku Jawdb changes

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,9 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-
+var jaws = false;
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
+  jaws = true;
 } else {
   connection = mysql.createConnection({
       host: "localhost",
@@ -12,6 +13,8 @@ if (process.env.JAWSDB_URL) {
   });
 };
 
+console.log (connection.database);
+
 // Make connection.
 connection.connect(function(err) {
   if (err) {
@@ -19,6 +22,21 @@ connection.connect(function(err) {
     return;
   }
   console.log("connected as id " + connection.threadId);
+  if (jaws) {
+    var queryString = `CREATE TABLE burgers
+    (
+      id int NOT NULL AUTO_INCREMENT,
+      burger_name varchar(255) NOT NULL,
+      devoured BOOLEAN DEFAULT false,
+      PRIMARY KEY (id)
+    );`;
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+        });
+  }
+  
 });
 
 // Export connection for our ORM to use.
--- models/burger.js
+++ models/burger.js
@@ -2,18 +2,18 @@ var orm = require("../config/orm.js");
 
 var burger = {
     selectAll: function(cb) {
-      orm.selectAll("burgers", function(res) {
+      orm.selectAll("z5dlstv4h99boly0", function(res) {
         cb(res);
       });
     },
     // The variables cols and vals are arrays.
     insertOne: function(burgerInput, cb) {
-      orm.insertOne("burgers", "burger_name", burgerInput, function(res) {
+      orm.insertOne("z5dlstv4h99boly0", "burger_name", burgerInput, function(res) {
         cb(res);
       });
     },
     updateOne: function(id, cb) {
-      orm.updateOne("burgers", "devoured", true, id, function(res) {
+      orm.updateOne("z5dlstv4h99boly0", "devoured", true, id, function(res) {
         cb(res);
       });
     }
